0583b5c887269ae48c1171f0909b30fb4c477353
Akshay Jain <104191134+AJ0202@users.noreply.github.com>
2022-04-22 17:05:31 +0530

    Update streamlit_app.py
    Emoji added

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,9 +2,10 @@ import streamlit
 
 streamlit.title('My Mom\'s New Healthy Diner')
 streamlit.header('Breakfast Favorites')
-streamlit.text('Omega 3 & Blueberry oatmeal')
-streamlit.text('Kale, Spinach, & Rocket Smoothie')
-streamlit.text('Hard-boiled Free-range Egg')
+streamlit.text('🥗Omega 3 & Blueberry oatmeal')
+streamlit.text('🌮Kale, Spinach, & Rocket Smoothie')
+streamlit.text('🦪Hard-boiled Free-range Egg')
+streamlit.text('🍞Avacodo Toast')
 
 streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
 
