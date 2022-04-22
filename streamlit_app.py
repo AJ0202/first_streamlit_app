5ca151ce845a1ba3c56c3d2fe1ec7f7219de73e6
Akshay Jain <104191134+AJ0202@users.noreply.github.com>
2022-04-22 16:46:41 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,6 @@
 import streamlit
 
-streamlit.title('My MOM\'s New Healthy Diner')
+streamlit.title('My Mom\'s New Healthy Diner')
 streamlit.title('Breakfast Menu')
 
 streamlit.title('Omega 3 & Blueberry oatmeal')
