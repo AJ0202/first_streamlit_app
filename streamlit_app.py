d4c12aa623d33b7583858104518961a98e57bd65
Akshay Jain <104191134+AJ0202@users.noreply.github.com>
2022-04-22 17:01:19 +0530

    Update streamlit_app.py
    Uppper case in header

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,7 @@
 import streamlit
 
 streamlit.title('My Mom\'s New Healthy Diner')
-streamlit.Header('Breakfast Favorites')
+streamlit.header('Breakfast Favorites')
 streamlit.text('Omega 3 & Blueberry oatmeal')
 streamlit.text('Kale, Spinach, & Rocket Smoothie')
 streamlit.text('Hard-boiled Free-range Egg')
